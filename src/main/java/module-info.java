6b74783c7f9ec12acbe861224848aae6d311464b
bin_tenkuu <2938137849@qq.com>
2024-07-23 17:35:38 +0800

    module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * @author bin
+ * @version 1.0.0
+ * @since 2024/07/23
+ */module demo {
+    requires static lombok;
+    requires static org.jetbrains.annotations;
+    requires jmh.core;
+    requires jdk.incubator.vector;
+    requires commons.math3;
+    requires java.desktop;
+    requires org.apache.lucene.core;
+    requires jol.core;
+}
