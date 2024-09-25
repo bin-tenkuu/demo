34e0a4e93a6065aeae0bfc9f912498f1876d7b9e
bin_tenkuu <2938137849@qq.com>
2024-09-25 08:51:41 +0800

    规范模块

---
+++ IEC104/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author bin
+ * @since 2024/07/23
+ */
+module demo {
+    requires static lombok;
+    requires static org.jetbrains.annotations;
+}
--- src/main/java/demo/IEC104/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * IEC104协议的实现
- *
- * @author bin
- * @version 1.0.0
- * @since 2024/09/23
- */
-package demo.IEC104;
