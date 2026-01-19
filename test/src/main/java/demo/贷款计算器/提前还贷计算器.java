7c2f24daf679a3f8d1a0439a3dcb9ea52c479adf
杨启东 <2938137849@qq.com>
2026-01-19 15:09:12 +0800

    提前还贷计算器.java

--- test/src/main/java/demo/贷款计算器/提前还贷计算器.java
+++ test/src/main/java/demo/贷款计算器/提前还贷计算器.java
@@ -1,5 +1,8 @@
 package demo.贷款计算器;
 
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
 import java.text.DecimalFormat;
 import java.time.YearMonth;
 
@@ -24,90 +27,109 @@ public class 提前还贷计算器 {
     private static final 还款方式E 还款方式 = 还款方式E.等额本金;
     private static final YearMonth 首次还款时间 = YearMonth.of(2026, 1);
     private static final 提前还款[] 提前还款方案 = {
-            new 提前还款(2026, 1, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 2, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 3, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 4, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 5, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 6, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 7, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 8, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 9, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 10, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 11, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2026, 12, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2027, 1, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2027, 2, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2027, 3, 10000, 提前还款方式E.减少月还款额),
-            new 提前还款(2027, 4, 10000, 提前还款方式E.缩短还款期限),
-            new 提前还款(2027, 5, 10000, 提前还款方式E.缩短还款期限),
+            new 提前还款(2026, 1, 2000, 提前还款方式E.减少月还款额),
     };
 
+    @AllArgsConstructor
     private static class 报告 {
-        public YearMonth 还款日期;
-        public int 剩余期数;
-        public double 还前本金;
-        public double 还后本金;
-        public double 偿还本金;
-        public double 偿还利息;
-        public double 还款金额;
+        public final YearMonth 还款日期;
+        public final int 剩余期数;
+        public final double 还前本金;
+        public final double 还后本金;
+        public final double 偿还本金;
+        public final double 偿还利息;
+        public final double 还款金额;
+
+        public 报告(YearMonth 还款日期, int 剩余期数, double 还前本金) {
+            this.还款日期 = 还款日期;
+            this.剩余期数 = 剩余期数;
+            this.还前本金 = 还前本金;
+            this.偿还本金 = 还前本金 / 剩余期数;
+            this.偿还利息 = 还前本金 * 贷款月利率;
+            this.还后本金 = 还前本金 - 偿还本金;
+            this.还款金额 = 偿还本金 + 偿还利息;
+        }
+
+        public 报告(YearMonth 还款日期, int 剩余期数, double 还前本金, double 偿还本金) {
+            this.还款日期 = 还款日期;
+            this.剩余期数 = 剩余期数;
+            this.还前本金 = 还前本金;
+            this.偿还本金 = 偿还本金;
+            this.偿还利息 = 0;
+            this.还后本金 = 还前本金 - 偿还本金;
+            this.还款金额 = 偿还本金;
+        }
 
         public 报告() {
-            还款日期 = 首次还款时间;
-            剩余期数 = 贷款期数;
-            还前本金 = 贷款金额;
-            偿还本金 = 还前本金 / 剩余期数;
-            偿还利息 = 还前本金 * 贷款月利率;
-            还后本金 = 还前本金 - 偿还本金;
-            还款金额 = 偿还本金 + 偿还利息;
+            this(首次还款时间, 贷款期数, 贷款金额);
         }
 
+        @NotNull
         public 报告 next() {
-            var nbg = new 报告();
-            nbg.还款日期 = 还款日期.plusMonths(1);
-            nbg.剩余期数 = 剩余期数 - 1;
-            nbg.还前本金 = 还后本金;
-            if (还款方式 == 还款方式E.等额本金) {
-                nbg.偿还本金 = nbg.还前本金 / nbg.剩余期数;
-                nbg.偿还利息 = nbg.还前本金 * 贷款月利率;
-                nbg.还后本金 = nbg.还前本金 - nbg.偿还本金;
-                nbg.还款金额 = nbg.偿还本金 + nbg.偿还利息;
+            if (剩余期数 <= 0 || 还后本金 < 0.01) {
+                return new 报告(
+                        还款日期.plusMonths(1),
+                        0,
+                        0
+                );
+            } else if (还款方式 == 还款方式E.等额本金) {
+                return new 报告(
+                        还款日期.plusMonths(1),
+                        剩余期数 - 1,
+                        还后本金
+                );
+            } else {
+                return null;
             }
-            return nbg;
         }
 
-        public void 提前还款(提前还款方式E 方式, double 提前还款金额) {
-            还前本金 = 还后本金;
-            还后本金 = 还前本金 - 提前还款金额;
-            if (还后本金 < 0.01) {
+        @NotNull
+        public 报告 提前还款(提前还款方式E 方式, double 提前还款金额) {
+            报告 nbg;
+            if (还后本金 <= 提前还款金额) {
                 // 提前结清
-                剩余期数 = 0;
-                偿还本金 = 0.0;
-                偿还利息 = 0.0;
-                还款金额 = 0.0;
                 System.out.printf("--->\t%s\t\t%9s\t\t已结清\n", 还款日期, f(提前还款金额));
-                return;
+                return new 报告(
+                        还款日期,
+                        0,
+                        还后本金,
+                        还后本金
+                );
             }
             switch (方式) {
                 case 减少月还款额 -> {
-                    System.out.printf("--->\t%s\t\t%9s\t\t(减少月还款额)\t\t\t\t\t%9s\t->\t%9s\n",
-                            还款日期, f(提前还款金额), f(还前本金), f(还后本金)
-                    );
+                    if (还款方式 == 还款方式E.等额本金) {
+                        nbg = new 报告(
+                                还款日期,
+                                剩余期数,
+                                还后本金 - 提前还款金额
+                        );
+                        var 减少月还款额 = 偿还本金 - nbg.偿还本金;
+                        System.out.printf("--->\t%s\t\t%9s\t\t(减少月还款额) 少 %s 元\t\t%9s\t->\t%9s\n",
+                                还款日期, f(提前还款金额), f(减少月还款额), f(nbg.还前本金), f(nbg.还后本金)
+                        );
+                        return nbg;
+                    }
                 }
                 case 缩短还款期限 -> {
                     if (还款方式 == 还款方式E.等额本金) {
                         // 计算新的剩余期数
                         var 新剩余期数 = (int) Math.ceil(还后本金 / (还款金额 - 还后本金 * 贷款月利率));
                         // 更新剩余期数
-                        var 原剩余期数 = 剩余期数;
                         // 确保新的期数至少为1
-                        剩余期数 = Math.max(1, 新剩余期数);
+                        nbg = new 报告(
+                                还款日期,
+                                Math.max(2, 新剩余期数),
+                                还后本金 - 提前还款金额
+                        );
                         System.out.printf(
                                 "--->\t%s\t\t%9s\t(缩短还款期限) 剩余期数 %d -> %d 期\t%9s\t->\t%9s\n",
-                                还款日期, f(提前还款金额), 原剩余期数, 剩余期数, f(还前本金), f(还后本金));
+                                还款日期, f(提前还款金额), 剩余期数, nbg.剩余期数, f(nbg.还前本金), f(nbg.还后本金));
+                        return nbg;
                     }
                 }
             }
+            return null;
         }
 
         public void print(int 期次) {
@@ -120,23 +142,26 @@ public class 提前还贷计算器 {
     static void main() {
         var index = 0;
         var 报告 = new 报告();
-        var 提前还款Index = 0;
+        // var 提前还款Index = 0;
         var 总利息 = 0.0;
+        var 存钱 = 2000.0;
         printHead();
-        index++;
-        报告.print(index);
-        总利息 += 报告.偿还利息;
-        while (报告.剩余期数 > 1) {
-            while (提前还款Index < 提前还款方案.length &&
-                    !报告.还款日期.isBefore(提前还款方案[提前还款Index].提前还款时间)) {
-                var 提前还款 = 提前还款方案[提前还款Index];
-                报告.提前还款(提前还款.提前还款方案, 提前还款.提前还款金额);
-                提前还款Index++;
-            }
-            报告 = 报告.next();
+        while (报告.剩余期数 > 0) {
             index++;
             报告.print(index);
             总利息 += 报告.偿还利息;
+            if (存钱 > 0) {
+                var next = 报告.提前还款(提前还款方式E.缩短还款期限, 存钱);
+                存钱 += 报告.偿还本金 - next.偿还本金;
+                报告 = next;
+            }
+            // while (提前还款Index < 提前还款方案.length &&
+            //         !报告.还款日期.isBefore(提前还款方案[提前还款Index].提前还款时间)) {
+            //     var 提前还款 = 提前还款方案[提前还款Index];
+            //     报告 = 报告.提前还款(提前还款.提前还款方案, 提前还款.提前还款金额);
+            //     提前还款Index++;
+            // }
+            报告 = 报告.next();
         }
         System.out.printf("总利息: %.2f 元\n", 总利息);
     }
@@ -160,8 +185,12 @@ public class 提前还贷计算器 {
         减少月还款额,
     }
 
-    private record 提前还款(YearMonth 提前还款时间, double 提前还款金额, 提前还款方式E 提前还款方案) {
-        public 提前还款(int year, int month, int 元, 提前还款方式E 提前还款方案) {
+    private record 提前还款(
+            YearMonth 提前还款时间,
+            double 提前还款金额,
+            提前还款方式E 提前还款方案
+    ) {
+        public 提前还款(int year, int month, double 元, 提前还款方式E 提前还款方案) {
             this(YearMonth.of(year, month), 元, 提前还款方案);
         }
     }
