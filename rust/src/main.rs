6e38f80a1c70d84d29bef81b99a64b493d0a4985
杨启东 <2938137849@qq.com>
2026-03-11 15:11:50 +0800

    学习 Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 
 // 进度：
+// https://www.zhihu.com/question/552471654/answer/1973850206132015365
 // https://kaisery.github.io/trpl-zh-cn/ch07-00-managing-growing-projects-with-packages-crates-and-modules.html
 fn main() {
     let config_max = Some(3u8);
