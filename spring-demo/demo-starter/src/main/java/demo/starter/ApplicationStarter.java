3cd18341aaf7aaf80193413778af0890abcadb24
杨启东 <2938137849@qq.com>
2026-04-14 09:39:38 +0800

    Refactor ApplicationStarter to remove unused imports and event listener

--- spring-demo/demo-starter/src/main/java/demo/starter/ApplicationStarter.java
+++ spring-demo/demo-starter/src/main/java/demo/starter/ApplicationStarter.java
@@ -1,10 +1,7 @@
 package demo.starter;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * @author bin
@@ -12,14 +9,8 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * @since 2024/11/11
  */
 @SpringBootApplication
-public class ApplicationStarter implements ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
-
-    @Override
-    public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
-        System.out.println("启动成功");
-    }
-
 }
